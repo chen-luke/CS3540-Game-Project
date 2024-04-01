36ed0869ccde2206fccf1f81bac164662e6c5a97
Aidan Rosenberg <aidan@rosenbergs.org>
2024-03-31 20:49:06 -0400

    npc animator

--- CS3540-Final-Game/Assets/Scripts/DialogueBox.cs
+++ CS3540-Final-Game/Assets/Scripts/DialogueBox.cs
@@ -68,17 +68,17 @@ public class DialogueBox : MonoBehaviour
             case 1:
                 currentDialogue = npc1_dialogue;
                 currentIndx = 0;
-                currentNPC = "GGG";
+                currentNPC = "Fundys";
                 break;
             case 2:
                 currentDialogue = npc2_dialogue;
                 currentIndx = 0;
-                currentNPC = "HHH";
+                currentNPC = "Ood";
                 break;
             case 3:
                 currentDialogue = npc3_dialogue;
                 currentIndx = 0;
-                currentNPC = "III";
+                currentNPC = "Al-Go";
                 break;
         }
     }
@@ -104,7 +104,7 @@ public class DialogueBox : MonoBehaviour
 
     private void PopulateNPC1()
     {
-        String panel1 = "Hello, explorer! You had quite a crash there. I'm so glad to see that you're okay. My name is GGG and I live here on Khoury.";
+        String panel1 = "Hello, explorer! You had quite a crash there. I'm so glad to see that you're okay. My name is Fundys and I live here on Khoury.";
         String panel2 = "I reckon you probably want to get back to your spaceship up there on that top island. Well, you'll need some help to do that.";
         String panel3 = "You're going to need to get up to the second island and you won't be able to do it without a pair of Super Boots.";
         String panel4 = "Lucky for you, there is a pair of Super Boots that will help you jump super high just along this path here.";
@@ -116,12 +116,12 @@ public class DialogueBox : MonoBehaviour
 
     private void PopulateNPC2()
     {
-        String panel1 = "Hello, explorer! I saw that crash, earlier. Oh, and it looks like you spoke to GGG already. I'm HHH and I live on Khoury, too.";
+        String panel1 = "Hello, explorer! I saw that crash, earlier. Oh, and it looks like you spoke to Fundys already. I'm Ood and I live on Khoury, too.";
         String panel2 = "I'm sure you want to get to your ship on that neighboring island, but there is a big wall of stone in the way.";
         String panel3 = "A normal attack won't make a dent in that wall, but there is a Super Glove on that platform floating above the lake that may be of use. It will allow you to do stronger attacks that can bring down that wall.";
-        String panel4 = "You'll have to be cautious, though! I'm assuming GGG told you about the corrupted spawners on the islands. Well, the spawners on this island have been corrupted, too. There are a bunch of angry creatures on this island that like to shoot fireballs.";
+        String panel4 = "You'll have to be cautious, though! I'm assuming Fundys told you about the corrupted spawners on the islands. Well, the spawners on this island have been corrupted, too. There are a bunch of angry creatures on this island that like to shoot fireballs.";
         String panel5 = "Good luck, explorer!";
-        npc2_dialogue = new string[] { panel1, panel2, panel3, panel4, panel4, panel5 };
+        npc2_dialogue = new string[] { panel1, panel2, panel3, panel4, panel5 };
     }
 
     private void PopulateNPC3()
@@ -129,8 +129,8 @@ public class DialogueBox : MonoBehaviour
         String panel1 = "H-h-h-hello, explorer! That m-m-must be your sh-sh-ship that crashed, earlier. I'm sure y-you want to g-g-get it back, but there's a bit of a p-p-problem.";
         String panel2 = "The c-c-corruption that infected the sp-spawners seems to have gotten to your sh-sh-ship too. It's not moving now, but y-y-you should have seen it when it got corrupted! Oh, it was a-a-a-awful.";
         String panel3 = "If you want to g-get your ship back, you'll n-n-need to defeat it first. It's not going to be easy, b-but I believe in you!";
-        String panel4 = "Oh, and I f-forgot to mention. M-my name is III. G-g-good luck, explorer!";
-        npc3_dialogue = new string[] { panel1, panel2, panel3, panel4, panel4 };
+        String panel4 = "Oh, and I f-forgot to mention. M-my name is Al-Go. G-g-good luck, explorer!";
+        npc3_dialogue = new string[] { panel1, panel2, panel3, panel4};
     }
 
 
--- CS3540-Final-Game/Assets/Scripts/NPCBehavior.cs
+++ CS3540-Final-Game/Assets/Scripts/NPCBehavior.cs
@@ -4,21 +4,45 @@ using UnityEngine;
 
 public class NPCBehavior : MonoBehaviour
 {
+    public enum RobotFSMStates {
+        Idle,
+        Scared
+    }
 
     public GameObject dialoguePanel;
     public int npcNumber;
     public bool canTalk = false;
+    private Animator anim;
+    public RobotFSMStates currentState = RobotFSMStates.Idle;
     private DialogueBox dialogue;
 
     // Start is called before the first frame update
     void Start()
     {
         dialogue = dialoguePanel.GetComponent<DialogueBox>();
+        anim = gameObject.GetComponent<Animator>();
+        // gameObject.GetComponent<CharacterController>().detectCollisions = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if(Input.GetKeyDown(KeyCode.X)) {
+            if(canTalk) {
+                dialogue.SetNPC(npcNumber);
+                dialogue.PauseGame();
+            }
+        }
+
+        switch(currentState) {
+            case RobotFSMStates.Idle:
+                UpdateIdleState();
+                break;
+            case RobotFSMStates.Scared:
+                UpdateScaredState();
+                break;
+        }
 
     }
 
@@ -26,11 +50,7 @@ public class NPCBehavior : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            dialogue.SetNPC(npcNumber);
             canTalk = true;
-            //dialogue.PauseGame();
-
-            //Destroy(gameObject.GetComponent<CapsuleCollider>());
         }
 
     }
@@ -40,5 +60,19 @@ public class NPCBehavior : MonoBehaviour
         if(other.CompareTag("Player")) {
            canTalk = false; 
         }
+    }
+
+    // public void OnCollisionEnter(Collision other) {
+    //     // gameObject.GetComponent<CharacterController>().detectCollisions = false;
+    // }
+
+    private void UpdateIdleState() {
+        anim.SetInteger("RobotAnimState", 0);
+        currentState = RobotFSMStates.Idle;
+    }
+
+    private void UpdateScaredState() {
+        anim.SetInteger("RobotAnimState", 1);
+        currentState = RobotFSMStates.Scared;
     }
 }
