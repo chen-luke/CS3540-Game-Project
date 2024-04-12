697429be7f26a0e8a654c6b0e8a47528ecde299c
Aidan Rosenberg <aidan@rosenbergs.org>
2024-04-11 21:48:09 -0400

    breakable rocks, updated main menu
    Added the rest of the actual breakable rocks. Added mouse sensitivity, volume, and invert controls to settings panel in main menu. Added about button and panel to main menu

--- CS3540-Final-Game/Assets/Scripts/Other/WaterBehavior.cs
+++ CS3540-Final-Game/Assets/Scripts/Other/WaterBehavior.cs
@@ -7,11 +7,21 @@ public class WaterBehavior : MonoBehaviour
     public AudioClip splashEnterSFX;
     public AudioClip splashExitSFX;
 
+    AudioSource source;
+
+    void Start() {
+        source = GetComponent<AudioSource>();
+        source.volume = .4f;
+        source.minDistance = .1f;
+        source.maxDistance = 10f;
+    }
+
     void OnTriggerEnter(Collider other) {
-        AudioSource.PlayClipAtPoint(splashEnterSFX, other.transform.position);
+        source.clip = splashEnterSFX;
+        AudioSource.PlayClipAtPoint(splashEnterSFX, other.transform.position, .5f);
     }
 
      void OnTriggerExit(Collider other) {
-        AudioSource.PlayClipAtPoint(splashExitSFX, other.transform.position);
+        AudioSource.PlayClipAtPoint(splashExitSFX, other.transform.position, .5f);
     }
 }
